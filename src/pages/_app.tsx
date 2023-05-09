ccf645642b8f52fafdbb8e7ca73b03aca49585cf
luongtuyenquang <luongtuyenquang77@gmail.com>
2023-05-09 23:25:50 +0700

    config tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import '../assets/css/tailwind.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -8,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="shop yolo" />
       </Head>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
---
+++ src/pages/admin/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+      </Head>
+
+      <h2 className="text-sky-500">Hello world</h2>
+    </>
+  );
+}
